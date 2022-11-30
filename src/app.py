30d0646b2dcceab1cc3598d82f5a329281a09b72
tim <timaltonji@protonmail.com>
2022-11-29 22:12:33 -0500

    flask

---
+++ src/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:@localhost/buy_book'
+
+
+
