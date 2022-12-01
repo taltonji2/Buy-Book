7c4ec967cf86366bbe1363e48b06d6acf184954a
tim <timaltonji@protonmail.com>
2022-11-30 21:09:24 -0500

    settings.ini

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from decouple import config
 
+config.encoding = 'cp1251'
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:@localhost/buy_book'
+app.config["SQLALCHEMY_DATABASE_URI"] = config('DATABASE_URL')
 
