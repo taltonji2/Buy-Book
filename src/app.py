c62ffb757beefc2848869fb32d20288bae9eb2db
tim <timaltonji@protonmail.com>
2022-12-07 21:52:23 -0500

    back on track

--- src/app.py
+++ src/app.py
@@ -1,69 +1,34 @@
 from flask import Flask, render_template, request
-from flask_restful import Api, Resource, reqparse
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask_mysqldb import MySQL
 from decouple import config
 
-config.encoding = 'cp1251'
+config.encoding = "cp1251"
 
 app = Flask(__name__)
-api = Api(app)
-app.config['SECRET_KEY'] = config('SECRET_KEY')
-app.config["SQLALCHEMY_DATABASE_URI"] = config('DATABASE_URL')
+mysql = MySQL()
 
-db = SQLAlchemy(app)
-# db Models
-class User(db.Model):
-    __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(25), nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+app.config["SECRET_KEY"] = config("SECRET_KEY")
+app.config["MYSQL_DATABASE_USER"] = "root"
+app.config["MYSQL_DATABASE_PASSWORD"] = config("DATABASE_PWD")
+app.config["MYSQL_DATABASE_HOST"] = "localhost"
+app.config["MYSQL_DATABASE_DB"] = "buy_book"
 
+mysql.init_app(app)
 
-class Bibliography(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    author = db.Column(db.String(50), nullable=False)
-    genre = db.Column(db.String(50))
-    description = db.Column(db.String(500))
-    publisher = db.Column(db.String(100))
-    publication_year = db.Column(db.Integer(4))
-    page_count = db.Column(db.Integer)
-    volume = db.Column(db.Integer)
-    rating = db.Column(db.Integer)
-
-
-class book (db.Model):
-    __tablename__ = 'book'
-    id = db.Column(db.Integer, primary_key=True)
-    bibliography_id = db.Column(db.Integer, db.ForeignKey('bibliography.id'), nullable=False)
-
-
-class ownership (db.Model):
-    __tablename__ = 'ownership'
-    id = db.Column(db.Integer, primary_key=True)
-    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeingKey('user.id'), nullable=False)
-
-class wishlist(db.Model):
-    __tablename__ = 'wishlist'
-    id = db.Column(db.Integer, primary_key=True)
-    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeingKey('user.id'), nullable=False)
+@app.route("/")
+def index():
+    data = "hello"
+    return render_template("index.html", data=data)
 
 
 
-# @app.route('/')
-# def index():
-
-
-# # localhost:5000/user/tim
-# @app.route('/user/<name>')
-# def user(name):
-
-
-# @app.route('/login', methods=['POST'])
-# def login():
+@app.route("/login")
+def login():
+    # conn = mysql.connect()
+    # cursor =conn.cursor()
+    # cursor.execute("SELECT * from User")
+    # data = cursor.fetchone()
+    return render_template("login.html")
 
 
 if __name__ == '__app__':
